810ddec5b10c2edfe86197dbcdc634c65f741fd4
baizinihao <xiaobaiebaizi@yeah.net>
2026-01-30 05:58:27 +0800

    Unzip baizi-plugin.zip: add guoba.support.js

---
+++ guoba.support.js
@@ -0,0 +1,14 @@
+export function supportGuoba() {
+    return {
+    pluginInfo: {
+        name: 'baizi-plugin',
+        title: 'baizi插件',
+        author: 'baizi',
+        authorLink: '无',
+        link: '不给🤓',
+        isV3: true,
+        isV2: false,
+        description: '一个适用于Yunzai-Bot的小功能插件'
+    }
+    }
+}
